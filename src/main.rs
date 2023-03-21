449ee552fc29030b2b404f4061764dce331a818a
Victor Trumpel <victor.trumpel@gmail.com>
2023-03-21 23:13:06 +0300

    add structs

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+struct _Socket {
+    description: String,
+}
+
+impl _Socket {
+    fn _get_description() -> String {
+        todo!()
+    }
+
+    fn _turn_on() {}
+    fn _turn_off() {}
+
+    fn _get_power() -> i32 {
+        todo!()
+    }
+}
+
+struct _Thermometer {
+    temperature: i32,
+}
+
+impl _Thermometer {
+    fn _get_temperature() -> i32 {
+        todo!()
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
